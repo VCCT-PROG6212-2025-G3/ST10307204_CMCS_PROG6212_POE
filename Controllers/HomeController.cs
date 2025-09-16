023c3f5491b136dbeaad08cda936d4abf727e6b7
LUI-B <lj3664901@gmail.com>
2025-09-17 00:28:51 +0200

    Adicionar arquivos de projeto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+using System.Diagnostics;
+using CMCS_PROG6212_POE.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CMCS_PROG6212_POE.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult LecturerDashboard()
+        {
+            return View();
+        }
+
+        public IActionResult ManagerDashboard()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
